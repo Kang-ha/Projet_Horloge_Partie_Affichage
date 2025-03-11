803b98a0d794a857e1e76d70eb614a20fa64635e
IannigAndre <andre.i1@stjolorient.fr>
2025-03-11 12:02:34 +0100

    Ajout des commentaire

--- Indication_affichage/Horloge.php
+++ Indication_affichage/Horloge.php
@@ -1,6 +1,4 @@
 <?php
-    ignore_user_abort(true);
-    set_time_limit(0);
     $serveurNom = "localhost:3306";
     $utilisateur = "admineleve";
     $motDePasse = "ieufdl";
@@ -10,6 +8,4 @@
         die("Connexion échouée: " . $conn->connect_error);
         console.log("Connexion échouée: " . $conn->connect_error);
     }
-    sleep(10);
-    file_put_contents("log.txt", "Exécution terminée\n", FILE_APPEND);
 ?>
